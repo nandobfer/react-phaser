c7c1b0d01f2b7e2ecee8fda755d47971e4698a43
nandobfer <nandobfer@yahoo.com.br>
2025-08-18 15:34:49 -0300

    chore: generalized ranged character

--- src/game/characters/Archer.ts
+++ src/game/characters/Archer.ts
@@ -1,58 +1,17 @@
 import { Arrow } from "../objects/Arrow"
 import { Game } from "../scenes/Game"
-import { Character } from "./Character"
+import { RangedCharacter } from "./RangedCharacter"
 
-export class Archer extends Character {
+export class Archer extends RangedCharacter<Arrow> {
     attackSpeed = 0.75
     speed = 40
     attackDamage = 30
-    attackRange = 5
 
     constructor(scene: Game, x: number, y: number) {
-        super(scene, x, y, "archer")
+        super(scene, x, y, "archer", Arrow)
     }
 
     extractAttackingAnimation() {
         this.extractAnimationsFromSpritesheet("attacking", 208, 13)
-    }
-
-    handleAnimationUpdate(_a: Phaser.Animations.Animation, _f: Phaser.Animations.AnimationFrame) {
-        /* no-op for Archer */
-    }
-
-    private fireArrow() {
-        if (!this.target || !this.target.active) return
-        const arrow = new Arrow(this)
-        arrow.fire()
-    }
-
-    handleAttack() {
-        if (this.isAttacking || !this.target?.active) {
-            return
-        }
-
-        this.updateFacingDirection()
-        this.isAttacking = true
-        const animKey = `${this.name}-attacking-${this.facing}`
-        const anim = this.anims.get(animKey)
-
-        const onUpdate = (animation: Phaser.Animations.Animation, frame: Phaser.Animations.AnimationFrame) => {
-            if (animation.key !== animKey) return
-            // Phaser frame indices are 1-based; we want "frame 9"
-            if (frame.index === 9) {
-                this.fireArrow()
-            }
-        }
-
-        this.on("animationupdate", onUpdate)
-
-        this.play({ key: animKey, frameRate: anim.frames.length * this.attackSpeed, repeat: 0 }, true)
-
-        const cleanup = () => {
-            this.off("animationupdate", onUpdate)
-            this.isAttacking = false
-        }
-        this.once("animationcomplete", cleanup)
-        this.once("animationstop", cleanup)
     }
 }
---
+++ src/game/characters/RangedCharacter.ts
@@ -0,0 +1,53 @@
+import { Projectile } from "../objects/Projectile"
+import { Game } from "../scenes/Game"
+import { Character } from "./Character"
+
+export class RangedCharacter<T extends Projectile> extends Character {
+    attackRange = 5
+    projectile: new (character: RangedCharacter<T>) => T
+
+    constructor(scene: Game, x: number, y: number, texture: string, projectile: new (character: RangedCharacter<T>) => T) {
+        super(scene, x, y, texture)
+        this.projectile = projectile
+    }
+
+    handleAnimationUpdate(_a: Phaser.Animations.Animation, _f: Phaser.Animations.AnimationFrame) {
+        /* no-op for ranged */
+    }
+
+    private fireProjectile() {
+        if (!this.target || !this.target.active) return
+        const projectile = new this.projectile(this)
+        projectile.fire()
+    }
+
+    handleAttack() {
+        if (this.isAttacking || !this.target?.active) {
+            return
+        }
+
+        this.updateFacingDirection()
+        this.isAttacking = true
+        const animKey = `${this.name}-attacking-${this.facing}`
+        const anim = this.anims.get(animKey)
+
+        const onUpdate = (animation: Phaser.Animations.Animation, frame: Phaser.Animations.AnimationFrame) => {
+            if (animation.key !== animKey) return
+            // Phaser frame indices are 1-based; we want "frame 9"
+            if (frame.index === 9) {
+                this.fireProjectile()
+            }
+        }
+
+        this.on("animationupdate", onUpdate)
+
+        this.play({ key: animKey, frameRate: anim.frames.length * this.attackSpeed, repeat: 0 }, true)
+
+        const cleanup = () => {
+            this.off("animationupdate", onUpdate)
+            this.isAttacking = false
+        }
+        this.once("animationcomplete", cleanup)
+        this.once("animationstop", cleanup)
+    }
+}
