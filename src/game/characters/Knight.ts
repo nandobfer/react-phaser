33e884de396081dc6075f9c16a0c3cfef294fcf4
nandobfer <nandobfer@yahoo.com.br>
2025-08-18 17:35:27 -0300

    stats

--- src/game/characters/Knight.ts
+++ src/game/characters/Knight.ts
@@ -2,7 +2,7 @@ import { Game } from "../scenes/Game"
 import { Character } from "./Character"
 
 export class Knight extends Character {
-    armor = 50
+    armor = 10
     resistance = 10
 
     constructor(scene: Game, x: number, y: number) {
