c95aa64ed50081571de81b9f8ed8b41393ae6412
nandobfer <nandobfer@yahoo.com.br>
2025-08-19 16:59:06 -0300

    clean

--- src/game/scenes/Game.ts
+++ src/game/scenes/Game.ts
@@ -27,25 +27,9 @@ export class Game extends Scene {
 
     create() {
         this.camera = this.cameras.main
-        // this.camera.setBackgroundColor(0x00ff00);
 
         this.createBackground()
         this.grid = new Grid(this, this.background)
-
-        // this.gameText = this.add
-        //     .text(512, 384, "Make something fun!\nand share it with us:\nsupport@phaser.io", {
-        //         fontFamily: "Arial Black",
-        //         fontSize: 38,
-        //         color: "#ffffff",
-        //         stroke: "#000000",
-        //         strokeThickness: 8,
-        //         align: "center",
-        //     })
-        //     .setOrigin(0.5)
-        //     .setDepth(100)
-
-        // const rogue = new Rogue(this, this.camera.width / 2, this.camera.height / 2)
-        // const archer = new Archer(this, this.camera.width / 2.2, this.camera.height / 1.8)
 
         this.playerTeam = new CharacterGroup(this, [], { isPlayer: true })
 
